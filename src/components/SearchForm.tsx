9ff21707043b93c53ae79af5bb946fd6d320c2ff
Emanuele Favero <emafaux89@gmail.com>
2025-09-15 20:58:10 +0200

    feat: style SearchForm

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -2,9 +2,20 @@ import { redirectWithSearch } from '@/app/actions'
 
 export default function SearchForm() {
   return (
-    <form action={redirectWithSearch}>
-      <input type='text' name='search' placeholder='Search word or phrase' />
-      <button type='submit'>Search</button>
+    <form action={redirectWithSearch} className='flex flex-wrap gap-1'>
+      <input
+        type='text'
+        name='search'
+        placeholder='Search word or phrase'
+        className='rounded-full border px-3 py-1'
+        required
+      />
+      <button
+        type='submit'
+        className='cursor-pointer rounded-full bg-foreground px-4 py-1 text-background transition duration-200 active:scale-95'
+      >
+        Search
+      </button>
     </form>
   )
 }

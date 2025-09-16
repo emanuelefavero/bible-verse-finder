83e739728f2daee89781f62b179e164d39323728
Emanuele Favero <emafaux89@gmail.com>
2025-09-16 23:54:15 +0200

    fix: body padding disappearing when clicking mode toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
         >
           <Header />
 
-          <main>{children}</main>
+          <main className='px-2 py-2'>{children}</main>
         </ThemeProvider>
       </body>
     </html>
--- src/features/layout/components/header.tsx
+++ src/features/layout/components/header.tsx
@@ -2,7 +2,7 @@ import { ModeToggle } from '@/features/theme/components/mode-toggle'
 
 export function Header() {
   return (
-    <header className='mb-8 flex items-center justify-between'>
+    <header className='mb-8 flex items-center justify-between px-2 py-2'>
       <h1 className='font-bold'>Bible Verse Finder</h1>
       <ModeToggle />
     </header>

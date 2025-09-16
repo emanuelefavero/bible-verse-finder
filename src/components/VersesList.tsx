3cfae018a02e37ee5adf70e22aec941b5c9c8b2f
Emanuele Favero <emafaux89@gmail.com>
2025-09-16 02:03:34 +0200

    feat: update VersesList with flex

--- src/components/VersesList.tsx
+++ src/components/VersesList.tsx
@@ -6,9 +6,12 @@ type Props = {
 
 export default function VersesList({ verses }: Props) {
   return (
-    <ul className='max-w-prose space-y-4 py-4'>
+    <ul className='flex max-w-prose flex-wrap gap-4 py-4'>
       {verses.map((verse) => (
-        <li key={verse.pk} className='rounded-md border p-4'>
+        <li
+          key={verse.pk}
+          className='rounded-md border border-foreground/20 p-4'
+        >
           <p className='mb-2 italic'>&quot;{verse.text}&quot;</p>
           <p className='text-sm text-gray-500'>
             {verse.translation} - Book {verse.book}, Chapter {verse.chapter},

37fce3e55c08052725d8d74a413d8007e2833faf
Emanuele Favero <emafaux89@gmail.com>
2025-09-16 15:55:34 +0200

    feat: add VerseItem component

---
+++ src/components/VerseItem.tsx
@@ -0,0 +1,17 @@
+import type { Verse } from '@/types/verses'
+
+type Props = {
+  verse: Verse
+}
+
+export default function VerseItem({ verse }: Props) {
+  return (
+    <li key={verse.pk} className='rounded-md border border-foreground/20 p-4'>
+      <p className='mb-2 italic'>&quot;{verse.text}&quot;</p>
+      <p className='text-sm text-gray-500'>
+        {verse.translation} - Book {verse.book}, Chapter {verse.chapter}, Verse{' '}
+        {verse.verse}
+      </p>
+    </li>
+  )
+}
--- src/components/VersesList.tsx
+++ src/components/VersesList.tsx
@@ -1,3 +1,4 @@
+import VerseItem from '@/components/VerseItem'
 import type { Verse } from '@/types/verses'
 
 type Props = {
@@ -8,16 +9,7 @@ export default function VersesList({ verses }: Props) {
   return (
     <ul className='flex max-w-prose flex-wrap gap-4 py-4'>
       {verses.map((verse) => (
-        <li
-          key={verse.pk}
-          className='rounded-md border border-foreground/20 p-4'
-        >
-          <p className='mb-2 italic'>&quot;{verse.text}&quot;</p>
-          <p className='text-sm text-gray-500'>
-            {verse.translation} - Book {verse.book}, Chapter {verse.chapter},
-            Verse {verse.verse}
-          </p>
-        </li>
+        <VerseItem key={verse.pk} verse={verse} />
       ))}
     </ul>
   )

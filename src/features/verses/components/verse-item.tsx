36e93f8e9719c525a70ddac86742e072c51780e0
Emanuele Favero <emafaux89@gmail.com>
2025-09-17 16:32:20 +0200

    feat: udpate verse-item style

--- src/features/verses/components/verse-item.tsx
+++ src/features/verses/components/verse-item.tsx
@@ -6,16 +6,16 @@ type Props = {
 
 export function VerseItem({ verse }: Props) {
   return (
-    <li key={verse.pk} className='rounded-md border border-foreground/20 p-4'>
+    <li key={verse.pk} className='rounded-md p-4'>
       {/* Text */}
-      <blockquote className='mb-2 italic'>
+      <blockquote className='mb-2 border-l-2 pl-6 italic'>
         <span aria-hidden='true'>&quot;</span>
         <span dangerouslySetInnerHTML={{ __html: verse.text }} />
         <span aria-hidden='true'>&quot;</span>
       </blockquote>
 
       {/* Citation */}
-      <p className='text-sm text-gray-500'>
+      <p className='text-sm text-muted-foreground'>
         {verse.translation} - Book {verse.book}, Chapter {verse.chapter}, Verse{' '}
         {verse.verse}
       </p>

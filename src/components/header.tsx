759db4033c6988d35bbfc83fb33c195a4d292e86
Emanuele Favero <emafaux89@gmail.com>
2025-09-18 14:45:52 +0200

    feat: add link to header logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,12 @@
 import { ModeToggle } from '@/features/theme/mode-toggle'
+import Link from 'next/link'
 
 export function Header() {
   return (
     <header className='flex items-center justify-between px-2 py-2'>
-      <span className='font-bold'>Bible Verse Finder</span>
+      <Link href='/' className='font-bold'>
+        Bible Verse Finder
+      </Link>
       <ModeToggle />
     </header>
   )

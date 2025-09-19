3f8c2fb5f1824d06a977dc941f405d1dd9beb692
Emanuele Favero <emafaux89@gmail.com>
2025-09-19 20:10:32 +0200

    feat: dynamically style verses-stats

--- src/features/verses/components/verses-stats.tsx
+++ src/features/verses/components/verses-stats.tsx
@@ -1,3 +1,5 @@
+import { cn } from '@/lib/utils'
+
 type Props = {
   exactMatches: number
   versesFound: number
@@ -6,8 +8,30 @@ type Props = {
 export function VersesStats({ exactMatches, versesFound }: Props) {
   return (
     <div className='mt-1 flex gap-4 text-xs font-semibold text-muted-foreground uppercase'>
-      <div>Exact matches: {exactMatches}</div>
-      <div>Verses found: {versesFound}</div>
+      <div>
+        Verses found:{' '}
+        <span
+          className={cn(
+            versesFound > 0
+              ? 'text-success-foreground'
+              : 'text-error-foreground',
+          )}
+        >
+          {versesFound}
+        </span>
+      </div>
+      <div>
+        Exact matches:{' '}
+        <span
+          className={cn(
+            exactMatches > 0
+              ? 'text-success-foreground'
+              : 'text-error-foreground',
+          )}
+        >
+          {exactMatches}
+        </span>
+      </div>
     </div>
   )
 }

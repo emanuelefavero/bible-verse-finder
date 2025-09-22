3843e5433d38bebd1744b345055fb5c1d6c65e79
Emanuele Favero <emafaux89@gmail.com>
2025-09-22 19:31:48 +0200

    feat: animate verses-stats

--- src/features/verses/components/verses-stats.tsx
+++ src/features/verses/components/verses-stats.tsx
@@ -1,5 +1,6 @@
 import { CountUp } from '@/components/ui/count-up'
 import { cn } from '@/lib/utils'
+import * as motion from 'motion/react-client'
 
 type Props = {
   exactMatches: number
@@ -18,34 +19,42 @@ export function VersesStats({ exactMatches, versesFound }: Props) {
 
   return (
     <div className='mt-1 flex gap-2 text-xs font-semibold text-muted-foreground uppercase'>
-      <div>
+      <motion.div
+        initial={{ opacity: 0, x: -72 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.2, ease: 'easeIn', delay: 0.1 }}
+      >
         Verses found:{' '}
         <CountUp
           to={versesFound}
           duration={0.5}
           stiffness={versesFoundStiffness}
           className={cn(
-            'inline-block min-w-[4ch] text-left ml-0.5 tabular-nums',
+            'ml-0.5 inline-block min-w-[4ch] text-left tabular-nums',
             versesFound > 0
               ? 'text-success-foreground'
               : 'text-error-foreground',
           )}
         />
-      </div>
-      <div>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, x: 72 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.2, ease: 'easeIn', delay: 0.1 }}
+      >
         Exact matches:{' '}
         <CountUp
           to={exactMatches}
           duration={0.5}
           stiffness={exactMatchesStiffness}
           className={cn(
-            'inline-block min-w-[4ch] text-left ml-0.5 tabular-nums',
+            'ml-0.5 inline-block min-w-[4ch] text-left tabular-nums',
             exactMatches > 0
               ? 'text-success-foreground'
               : 'text-error-foreground',
           )}
         />
-      </div>
+      </motion.div>
     </div>
   )
 }

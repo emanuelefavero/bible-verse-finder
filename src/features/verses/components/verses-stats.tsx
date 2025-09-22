91c05b2b191fbc1db91e660448bedd0ec683fa36
Emanuele Favero <emafaux89@gmail.com>
2025-09-22 14:19:14 +0200

    feat(verses-stats): assign slower animation to the bigger value

--- src/features/verses/components/verses-stats.tsx
+++ src/features/verses/components/verses-stats.tsx
@@ -7,6 +7,15 @@ type Props = {
 }
 
 export function VersesStats({ exactMatches, versesFound }: Props) {
+  // Assign a slower animation (smaller stiffness) to the bigger value
+  const higherStiffness = 300
+  const lowerStiffness = 200
+  const biggerValue = Math.max(exactMatches, versesFound)
+  const versesFoundStiffness =
+    versesFound === biggerValue ? lowerStiffness : higherStiffness
+  const exactMatchesStiffness =
+    exactMatches === biggerValue ? lowerStiffness : higherStiffness
+
   return (
     <div className='mt-1 flex gap-4 text-xs font-semibold text-muted-foreground uppercase'>
       <div>
@@ -14,7 +23,7 @@ export function VersesStats({ exactMatches, versesFound }: Props) {
         <CountUp
           to={versesFound}
           duration={0.5}
-          stiffness={300}
+          stiffness={versesFoundStiffness}
           className={cn(
             versesFound > 0
               ? 'text-success-foreground'
@@ -27,7 +36,7 @@ export function VersesStats({ exactMatches, versesFound }: Props) {
         <CountUp
           to={exactMatches}
           duration={0.5}
-          stiffness={200}
+          stiffness={exactMatchesStiffness}
           className={cn(
             exactMatches > 0
               ? 'text-success-foreground'

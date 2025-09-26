e967d4cbca891268ee4049caf30738f057859457
Emanuele Favero <emafaux89@gmail.com>
2025-09-26 22:36:13 +0200

    fix: type error in header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,7 @@
 import { ModeToggle } from '@/features/theme/mode-toggle'
 import { TranslationSelect } from '@/features/translation/components/translation-select'
 import languages from '@/features/translation/data/languages.json'
+import type { Language } from '@/features/translation/types'
 import { Logo } from './logo'
 
 export function Header() {
@@ -8,7 +9,7 @@ export function Header() {
     <header className='flex items-center justify-between px-2 py-2'>
       <Logo />
       <div className='flex items-center gap-2 rounded-md'>
-        <TranslationSelect languages={languages} />
+        <TranslationSelect languages={languages as Language[]} />
         <ModeToggle />
       </div>
     </header>

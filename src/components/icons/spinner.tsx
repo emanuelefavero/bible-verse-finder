84f78f06e93299c0140ee9af333db2288b591ea8
Emanuele Favero <emafaux89@gmail.com>
2025-10-07 20:02:58 +0200

    feat: make spinner component reusable

--- src/components/icons/spinner.tsx
+++ src/components/icons/spinner.tsx
@@ -1,12 +1,22 @@
-export function Spinner() {
+import { cn } from '@/lib/utils'
+
+type Props = React.ComponentPropsWithRef<'svg'> & {
+  label?: string
+}
+
+export function Spinner({ className, label = 'Loading...', ...props }: Props) {
   return (
     <div role='status'>
       <svg
         aria-hidden='true'
-        className='h-8 w-8 animate-spin fill-foreground text-muted'
+        className={cn(
+          'h-8 w-8 animate-spin fill-foreground text-muted',
+          className,
+        )}
         viewBox='0 0 100 101'
         fill='none'
         xmlns='http://www.w3.org/2000/svg'
+        {...props}
       >
         <path
           d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
@@ -17,7 +27,7 @@ export function Spinner() {
           fill='currentFill'
         />
       </svg>
-      <span className='sr-only'>Loading...</span>
+      <span className='sr-only'>{label}</span>
     </div>
   )
 }
